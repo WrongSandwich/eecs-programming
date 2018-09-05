07f55f4be7cd08414e9e15b8cb82b9b088673983
Evan Trout <fishyevan@gmail.com>
2018-09-05 12:33:48 -0500

    Add condition comments to Directory header

--- EECS_268/Lab01/Directory.h
+++ EECS_268/Lab01/Directory.h
@@ -9,7 +9,14 @@ private:
   std::string fileName;
   Course* catalog;
 public:
+  /* @pre fileName is valid
+  * @post Reads given file to an array of Courses, then closes the file and starts run()
+  */
   Directory(std::string fileName);
+
+  /* @pre None
+  * @post Runs the user menu and handles interactions with user and courseSize
+  */
   void run();
 };
 
