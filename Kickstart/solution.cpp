6cf734d94e15e648db04684de854d56c3758a85c
Evan Trout <fishyevan@gmail.com>
2018-09-10 12:57:15 -0500

    Add Kickstart directory and first problem

---
+++ Kickstart/solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int t, n, p, x, count;
+	int* bus = nullptr;
+	cin >> t; //Number of test cases
+	for(int i = 1; i <= t; i++)
+	{
+		cout << "Case #" << i << ": ";
+		cin >> n;
+		n = n*2;
+		bus = new int[n];
+		for (int j = 0; j < n; i++)
+		{
+			cin >> bus[j];
+		}
+		cin >> p;
+		for (int j = 0; j < p; j++)
+		{
+			cin >> x;
+			count = 0;
+			for (int a = 0; a < n; a = a+2)
+			{
+				if (x >= bus[a] && x <= bus[a+1])
+				{
+					count++;
+				}
+			}
+			cout << count << " ";
+		}
+		cout << '\n';
+		delete[] bus;
+		bus = nullptr;
+	}
+	return 0;
+}
