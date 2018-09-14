43ef2c7398eaaf86b5847def41051fe5d0bc2808
Evan Trout <fishyevan@gmail.com>
2018-09-14 13:38:36 -0500

    Write remaining LinkedList member functions

--- EECS_268/Lab02/LinkedList.cpp
+++ EECS_268/Lab02/LinkedList.cpp
@@ -44,6 +44,12 @@ bool LinkedList<itemType>::isEmpty() const;
 }
 
 template <class itemType>
+int LinkedList<itemType>::getLength() const;
+{
+  return itemCount;
+}
+
+template <class itemType>
 void LinkedList<T>::insert(int newPosition, const itemType& newEntry) throw (PrecondViolatedExcep)
 {
   //Must first confirm that insertion is valid (pos is not too high or low)
@@ -64,11 +70,78 @@ void LinkedList<T>::insert(int newPosition, const itemType& newEntry) throw (Pre
     }
     else
     {
-      Node<itemType>* prev = getNodeAt(pos-1);
+      Node<ItemType>* prev = getNodeAt(pos-1);
       n->setNext(prev->getNext()); //Setting n's pointer to next item in list
       prev->setNext(n);//Setting prev's pointer to point to new addition
       itemCount++;
     }
   }
   else throw PrecondViolatedExcep("error");
+
+template <class ItemType>
+void LinkedList<ItemType>::remove(int position) throw (PrecondViolatedExcep)
+{
+  if (position > 0) && (position <= itemCount) //Checking that position is valid
+  {
+    if (isEmpty = true) //List is empty
+    {
+      throw PrecondViolatedExcep("error")
+    }
+    else if (position == 1)
+    {
+      Node<ItemType>* newHead = getNodeAt(position+1); //getNode in position 2
+      delete[] headPtr; //delete current head
+      headPtr = newHead; //Make second position new head
+      itemCount = itemCount - 1;
+    }
+    else
+    {
+      Node<ItemType>* prev = getNodeAt(position-1);
+      Node<ItemType>* next = getNodeAt(position+1);
+      Node<ItemType>* target = getNodeAt(position);
+      delete[] target;
+      prev->setNext(next);
+      itemCount = itemCount - 1;
+    }
+  }
+  else throw PrecondViolatedExcep("error");
+}
+
+template <class ItemType>
+void LinkedList<itemType>::clear() //Removes all entries in the list
+{
+  Node<ItemType>* nodeToDeletePtr = headPtr;
+  while (headPtr != nullptr)
+  {
+    headPtr = headPtr->getNext(); //Move headPtr to next node
+    nodeToDeletePtr->setNext(nullptr); //Clear ptr of target node
+    delete nodeToDeletePtr; //Delete node contents
+    nodeToDeletePtr = headPtr; //Bring nodeToDeletePtr up to headPtr
+  }
+  itemCount = 0;
+}
+
+template <class ItemType>
+ItemType LinkedList<ItemType>::getEntry(int position) const throw (PrecondViolatedExcep)
+{
+  if (position > 0) && (position <= itemCount)
+  {
+    Node<ItemType>* targetNode = getNodeAt(position);
+    return targetNode->getItem();
+  }
+  else throw PrecondViolatedExcep("error");
+}
+
+template <class ItemType>
+void LinkedList<ItemType>::setEntry(int position, const ItemType& newEntry)
+                                    throw (PrecondViolatedExcep)
+{
+  if (position > 0) && (position <= itemCount)
+  {
+    Node<ItemType>* targetNode = getNodeAt(position);
+    targetNode->setItem(newEntry);
+  }
+  else throw PrecondViolatedExcep("error");
+}
+
 }
