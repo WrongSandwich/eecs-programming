816ced277b501f5b709081b1ab2c636daf7ff129
Evan Trout <fishyevan@gmail.com>
2018-09-22 10:30:50 -0500

    Add infinite loop to TCP client

--- EECS_563/Trout_TCP_Client.py
+++ EECS_563/Trout_TCP_Client.py
@@ -1,10 +1,15 @@
 from socket import *
-serverName = 'servername'
-serverPort = 12000
+serverPort = ''
+serverName = ''
+serverName = raw_input("Please enter a valid server address: ")
+while (len(serverPort) < 4):
+    serverPort = raw_input("Please enter a valid port of at least 4 digits: ")
+serverPort = int(serverPort)
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From server: ', modifiedSentence.decode())
+while True:
+    sentence = input('Input lowercase sentence:')
+    clientSocket.send(sentence.encode())
+    modifiedSentence = clientSocket.recv(1024)
+    print('From server: ', modifiedSentence.decode())
 clientSocket.close()
