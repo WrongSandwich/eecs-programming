70a94caff3d0821517102804374ef81033619a78
Evan Trout <fishyevan@gmail.com>
2018-09-21 21:39:30 -0500

    Add changes to UDP socket programs

--- EECS_563/Trout_UDP_Client.py
+++ EECS_563/Trout_UDP_Client.py
@@ -1,6 +1,12 @@
 from socket import *
+serverPort = ''
+serverName = ''
+serverName = raw_input("Please enter a valid server address: ")
+while (len(serverPort) < 4):
+    serverPort = raw_input("Please enter a valid port of at least 4 digits: ")
+serverPort = int(serverPort)
+print serverPort
 serverName = 'hostname'
-serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
