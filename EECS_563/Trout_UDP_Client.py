2b053969ffd19d977645425b6df5d4d0b3c65207
Evan Trout <fishyevan@gmail.com>
2018-09-22 10:32:17 -0500

    Add infinite loop to UDP Client

--- EECS_563/Trout_UDP_Client.py
+++ EECS_563/Trout_UDP_Client.py
@@ -5,10 +5,10 @@ serverName = raw_input("Please enter a valid server address: ")
 while (len(serverPort) < 4):
     serverPort = raw_input("Please enter a valid port of at least 4 digits: ")
 serverPort = int(serverPort)
-print serverPort
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+while True:
+    message = raw_input('Input lowercase sentence:')
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print(modifiedMessage.decode())
 clientSocket.close()
