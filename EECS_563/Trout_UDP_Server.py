d480f0876d30e381b3983457ace3afc9c52a0cb0
Evan Trout <fishyevan@gmail.com>
2018-09-22 10:32:46 -0500

    Remove unnecessary line from UDP Server

--- EECS_563/Trout_UDP_Server.py
+++ EECS_563/Trout_UDP_Server.py
@@ -3,7 +3,6 @@ serverPort = ''
 while (len(serverPort) < 4):
     serverPort = raw_input("Please enter a valid port of at least 4 digits: ")
 serverPort = int(serverPort)
-print serverPort
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
