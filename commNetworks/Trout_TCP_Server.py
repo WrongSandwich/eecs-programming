8daf7fce3312fe4d863fb2721348e6c9606fe2d1
Evan Trout <fishyevan@gmail.com>
2018-09-24 17:20:46 -0500

    Add changes to TCP files so program works

--- commNetworks/Trout_TCP_Server.py
+++ commNetworks/Trout_TCP_Server.py
@@ -9,9 +9,10 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print('The server is ready to receive')
+connectionSocket, addr = serverSocket.accept()
 while True:
-    connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
+    print sentence
     capitalizedSentence = sentence.upper()
     connectionSocket.send(capitalizedSentence.encode())
 connectionSocket.close()
