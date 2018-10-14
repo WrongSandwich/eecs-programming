a87ddcca6698993df174cc7eff0ee8c3ecc45e82
Evan Trout <fishyevan@gmail.com>
2018-10-14 10:39:35 -0500

    Write and implement methods for ProductionRule class

---
+++ projects2/Lab06/ProductionRule.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <random>
+#include <vector>
+#include "ProductionRule.h"
+
+ProductionRule::ProductionRule(std::string newLabel) : label(newLabel)
+{
+}
+
+ProductionRule::~ProductionRule()
+{
+  rules.clear();
+}
+
+std::string ProductionRule::getLabel()
+{
+  return label;
+}
+
+std::string ProductionRule::getRandRule()
+{
+  int max = (rules.size() - 1);
+  //See https://stackoverflow.com/questions/5008804/generating-random-integer-from-a-range
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, max);
+  int loc = dis(gen);
+  //Returning the rules entry with the random index generated
+  return rules[loc];
+}
+
+void ProductionRule::addrule(std::string newRule)
+{
+  rules.push_back(newRule);
+}
--- projects2/Lab06/ProductionRule.h
+++ projects2/Lab06/ProductionRule.h
@@ -1,12 +1,20 @@
 #ifndef PRODUCTION_RULE_H
 #define PRODUCTION_RULE_H
 
+#include <string>
+#include <vector>
+
 class ProductionRule
 {
 private:
-  
+  std::string label;
+  std::vector<std::string> rules;
 public:
-
+  ProductionRule(std::string newLabel);
+  ~ProductionRule();
+  std::string getLabel();
+  std::string getRandRule();
+  void addRule(std::string newRule);
 };
 
 #endif
