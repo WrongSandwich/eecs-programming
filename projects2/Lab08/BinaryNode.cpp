6fc421c7ad66934e530859c63fe0978ea4af3a0c
Evan Trout <fishyevan@gmail.com>
2018-11-13 11:25:33 -0600

    Write BinaryNode.cpp

---
+++ projects2/Lab08/BinaryNode.cpp
@@ -0,0 +1,64 @@
+#include "BinaryNode.h"
+
+//CONSTRUCTORS
+
+template<class ItemType>
+BinaryNode<ItemType>::BinaryNode() : leftchildPtr(nullptr), rightChildPtr(nullptr)
+{}
+
+template<class ItemType>
+BinaryNode<ItemType>::BinaryNode(const ItemType& anItem) : item(anItem),
+                                leftChildPtr(nullptr), rightChildPtr(nullptr)
+{}
+
+template<class ItemType>
+BinaryNode<ItemType>::BinaryNode(const ItemType& anItem, BinaryNode<ItemType>* leftPtr,
+                                BinaryNode<ItemType>* rightPtr)
+{
+  item = anItem;
+  leftChildPtr = leftPtr;
+  rightChildPtr = rightPtr;
+}
+
+//OTHER METHODS
+template<class ItemType>
+void BinaryNode<ItemType>::setItem(const ItemType& anItem)
+{
+  item = anItem;
+}
+
+template<class ItemType>
+ItemType BinaryNode<ItemType>::getItem() const
+{
+  return item;
+}
+
+template<class ItemType>
+bool BinaryNode<ItemType>::isLeaf() const //Determines if this node is a dead end
+{
+  return ((leftChildPtr == nullptr) && (rightChildPtr == nullptr))
+}
+
+template<class ItemType>
+BinaryNode<ItemType>* BinaryNode<ItemType>::getLeftChildPtr() const
+{
+  return leftChildPtr;
+}
+
+template<class ItemType>
+BinaryNode<ItemType>* BinaryNode<ItemType>::getRightChildPtr() const
+{
+  return rightChildPtr;
+}
+
+template<class ItemType>
+void BinaryNode<ItemType>::setLeftChildPtr(BinaryNode<ItemType>* leftPtr)
+{
+  leftChildPtr = leftPtr;
+}
+
+template<class ItemType>
+void BinaryNode<ItemType>::setRightChildPtr(BinaryNode<ItemType>* rightPtr)
+{
+  rightChildPtr = rightPtr;
+}
