ba65b55e5aacb57673daf05929397ec1a3e5c30f
Evan Trout <fishyevan@gmail.com>
2018-11-15 11:36:48 -0600

    Write first draft of Task class

---
+++ projects2/Lab09/Task.cpp
@@ -0,0 +1,39 @@
+
+
+Task::Task(int newID, std::string newName, int newEstimate, int currentTime)
+{
+  taskID = newID;
+  taskName = newName;
+  estimatedTimeToComplete = newEstimate;
+  timeAddedToBST = currentTime;
+  timeStarted = -1;
+}
+
+void Task::printStats()
+{
+  if (timeStarted == -1)
+  {
+    std::cout << "Task ID: " << taskID << '\n';
+    std::cout << "Task Name: " << taskName << '\n';
+    std::cout << "Time added to tree: " << timeAddedToBST << '\n';
+    std::cout << "Task has not yet started\n";
+  }
+  else
+  {
+    std::cout << "Task ID: " << taskID << '\n';
+    std::cout << "Task Name: " << taskName << '\n';
+    std::cout << "Time added to tree: " << timeAddedToBST << '\n';
+    std::cout << "Time started: " << timeStarted << '\n';
+    std::cout << "Estimated time to completion: " << estimatedTimeToComplete << '\n';
+  }
+}
+
+int Task::getID()
+{
+  return taskID;
+}
+
+void Task::startTask(int currentTime)
+{
+  timeStarted = currentTime;
+}
--- projects2/Lab09/Task.h
+++ projects2/Lab09/Task.h
@@ -12,5 +12,9 @@ private:
   int timeAddedToBST;
   int timeStarted;
 public:
+  Task(int newID, std::string newName, int newEstimate, int currentTime);
+  void printStats();
+  void getID();
+  void startTask(int currentTime);
 };
 #endif
