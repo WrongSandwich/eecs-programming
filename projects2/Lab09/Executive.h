ae49b230e3c98bc81c323ea158f9f6c759ba76aa
Evan Trout <fishyevan@gmail.com>
2018-11-15 11:11:51 -0600

    Add remaining blank files needed for lab09

---
+++ projects2/Lab09/Executive.h
@@ -0,0 +1,10 @@
+#ifndef EXECUTIVE
+#define EXECUTIVE
+
+class Executive
+{
+private:
+public:
+  Executive(std::string fileName);
+};
+#endif
---
+++ projects2/Lab09/Task.h
@@ -0,0 +1,16 @@
+#ifndef TASK
+#define TASK
+
+#include <string>
+
+class Task
+{
+private:
+  int taskID;
+  std::string taskName;
+  int estimatedTimeToComplete;
+  int timeAddedToBST;
+  int timeStarted;
+public:
+};
+#endif
