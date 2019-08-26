483a3891216f318729bfd4ba77802fdc7ef60647
Evan Trout <fishyevan@gmail.com>
2019-08-26 09:25:32 -0500

    Add main and Exec based off Lab09

---
+++ dataStructures/lab01/main.cpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    main.cpp
+*@date    09/21/2018
+*@brief   Main file, calls the executive class's constructor and handles any
+*         catches any exceptions.
+*******************************************************************************/
+
+#include <iostream>
+#include "Executive.h"
+#include <string>
+#include <stdexcept>
+
+int main(int argc, char* argv[])
+{
+  if(argc < 2)
+  {
+    std::cout << "Incorrect number of parameters!\n";
+  }
+  else
+  {
+    try {
+      Executive ex(argv[1]); //creation of executive object
+    }
+    catch(std::exception const& e)
+    {
+      std::cout << "Error: " << e.what() << '\n';
+    }
+  }
+  return(0);
+}
