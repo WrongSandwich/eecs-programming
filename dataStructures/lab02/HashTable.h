3a035a11aa772d0ff86c5a8aa3bb2385e2c015ad
Evan Trout <fishyevan@gmail.com>
2019-09-09 09:59:42 -0500

    Write first draft of HashTable.h

---
+++ dataStructures/lab02/HashTable.h
@@ -0,0 +1,33 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    HashTable.h
+*@date    09/09/2019
+*@brief   Header file for HashTable class. Creates and maintains a
+*         hash table of strings.
+*******************************************************************************/
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <string>
+#include <stdexcept>
+
+class HashTable
+{
+private:
+  LinkedList<std::string>* table;
+  int numEntries;
+  int bucketSize;
+public:
+  HashTable();
+  HashTable(std::string[] entries, int n);
+  ~HashTable();
+
+  bool insert(std::string newEntry);
+  bool delete(std::string target);
+  void print();
+  int hashFunction(std::string target);
+  void rehash();
+  int find(std::string key);
+  int getNextPrime(int n);
+}
