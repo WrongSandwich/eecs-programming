05685a0e4ea626ee43836eabb56d351a5cf036c1
Evan Trout <fishyevan@gmail.com>
2019-09-25 08:43:26 -0500

    Update BST header file

--- dataStructures/lab04/BinarySearchTree.h
+++ dataStructures/lab04/BinarySearchTree.h
@@ -10,15 +10,24 @@ public:
   BinarySearchTree();
   bool isEmpty() const;
   bool isFull() const;
+  bool fullHelper(BinaryNode<int>* subTreePtr);
   bool addItem(int x);
-  int delete();
+  bool addLevelOrder(BinaryNode<int>* subTreePtr, int x, int level);
+  int remove();
   bool leaf(int x) const;
   void printLeaf() const;
+  void leafPrinter(BinaryNode<int>* treePtr) const;
   void printTreeHeight() const;
+  int heightHelper(BinaryNode<int>* subTreePtr) const;
+  static void printNode(int x);
   void preorder() const;
+  void preorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
   void postorder() const;
+  void postorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
   void inorder() const;
+  void inorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
   void levelorder() const;
+  void printLevel(BinaryNode<int>* subTreePtr, int level) const;
 };
 
 #endif;
