a3a605ed07a03298f7f72ded8af12a87a25c7dc7
Evan Trout <fishyevan@gmail.com>
2019-09-30 08:55:43 -0500

    Copy lab4 files into lab5 directory

---
+++ dataStructures/lab05/BinaryTree.cpp
@@ -0,0 +1,336 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    BinaryTree.cpp
+*@date    09/25/19
+*@brief   Implementation file for BinaryTree class, which controls a tree of linked
+*         BinaryNodes and allows for various operations on them.
+*******************************************************************************/
+
+#include <iostream>
+#include "BinaryNode.h"
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree()
+{
+  rootPtr = nullptr;
+}
+
+BinaryTree::~BinaryTree()
+{
+  destroyTree(rootPtr);
+}
+
+void BinaryTree::destroyTree(BinaryNode<int>* subTreePtr)
+{
+  if (subTreePtr != nullptr)
+  {
+    destroyTree(subTreePtr->getLeftChildPtr());
+    destroyTree(subTreePtr->getRightChildPtr());
+    delete subTreePtr;
+  }
+}
+
+bool BinaryTree::isEmpty() const
+{
+  return (rootPtr == nullptr);
+}
+
+bool BinaryTree::isFull() const
+{
+  if (isEmpty())
+  {
+    return false;
+  }
+  else
+  {
+    return fullHelper(rootPtr);
+  }
+}
+
+bool BinaryTree::fullHelper(BinaryNode<int> *subTreePtr) const
+{
+  if ((subTreePtr->getLeftChildPtr() == nullptr) && (subTreePtr->getRightChildPtr() == nullptr))
+  {
+    return true;
+  }
+  else if ((subTreePtr->getLeftChildPtr() != nullptr) && (subTreePtr->getRightChildPtr() != nullptr))
+  {
+    bool leftFull = fullHelper(subTreePtr->getLeftChildPtr());
+    bool rightFull = fullHelper(subTreePtr->getRightChildPtr());
+    return (leftFull && rightFull);
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool BinaryTree::addItem(int x)
+{
+  if (rootPtr == nullptr)
+  {
+    rootPtr = new BinaryNode<int>(x);
+    std::cout << x << " added as root\n";
+    return true;
+  }
+  else if (findNode(rootPtr, x) != nullptr)
+  {
+    return false;
+  }
+  else
+  {
+    bool success = false;
+    int level = 0;
+    while (!success)
+    {
+      level++;
+      success = addLevelOrder(rootPtr, x, level);
+    }
+  }
+}
+
+bool BinaryTree::addLevelOrder(BinaryNode<int> *subTreePtr, int x, int level)
+{
+  if (level == 1)
+  {
+    if (subTreePtr->getLeftChildPtr() == nullptr)
+    {
+      BinaryNode<int> *tempPtr = new BinaryNode<int>(x);
+      subTreePtr->setLeftChildPtr(tempPtr);
+      tempPtr = nullptr;
+      return true;
+    }
+    else if (subTreePtr->getRightChildPtr() == nullptr)
+    {
+      BinaryNode<int> *tempPtr = new BinaryNode<int>(x);
+      subTreePtr->setRightChildPtr(tempPtr);
+      tempPtr = nullptr;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  if (level > 1)
+  {
+    if (addLevelOrder(subTreePtr->getLeftChildPtr(), x, level - 1))
+    {
+      return true;
+    }
+    else if (addLevelOrder(subTreePtr->getRightChildPtr(), x, level - 1))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
+
+int BinaryTree::remove()
+{
+  int level = heightHelper(rootPtr) - 1;
+  if (level == 0)
+  {
+    return -1;
+  }
+  return removeHelper(rootPtr, level);
+}
+
+int BinaryTree::removeHelper(BinaryNode<int>* subTreePtr, int level)
+{
+  if (level == 1)
+  {
+    if (subTreePtr->getRightChildPtr() != nullptr)
+    {
+      BinaryNode<int>* tempPtr = subTreePtr->getRightChildPtr();
+      int target = tempPtr->getItem();
+      delete tempPtr;
+      subTreePtr->setRightChildPtr(nullptr);
+      tempPtr = nullptr;
+      return target;
+    }
+    else if (subTreePtr->getLeftChildPtr() != nullptr)
+    {
+      BinaryNode<int>* tempPtr = subTreePtr->getLeftChildPtr();
+      int target = tempPtr->getItem();
+      delete tempPtr;
+      subTreePtr->setLeftChildPtr(nullptr);
+      tempPtr = nullptr;
+      return target;
+    }
+    else
+    {
+      return -1;
+    }
+  }
+  if (level > 1)
+  {
+    int result = removeHelper(subTreePtr->getRightChildPtr(), level - 1);
+    if (result != -1)
+    {
+      return result;
+    }
+    result = removeHelper(subTreePtr->getLeftChildPtr(), level - 1);
+    if (result != -1)
+    {
+      return result;
+    }
+    return -1;
+  }
+}
+
+bool BinaryTree::leaf(int x) const
+{
+  BinaryNode<int> *target = findNode(rootPtr, x);
+  return ((target->getLeftChildPtr() == nullptr) && (target->getRightChildPtr() == nullptr));
+}
+
+BinaryNode<int> *BinaryTree::findNode(BinaryNode<int> *subTreePtr, int x) const
+{
+  if (subTreePtr->getItem() == x)
+  {
+    return subTreePtr;
+  }
+  else if (subTreePtr->getRightChildPtr() == nullptr && subTreePtr->getLeftChildPtr() == nullptr)
+  {
+    return nullptr;
+  }
+  else
+  {
+    BinaryNode<int> *temp = findNode(subTreePtr->getLeftChildPtr(), x);
+    if (temp != nullptr)
+    {
+      return temp;
+    }
+    else if (subTreePtr->getRightChildPtr() != nullptr)
+    {
+      return findNode(subTreePtr->getRightChildPtr(), x);
+    }
+    else
+    {
+      return nullptr;
+    }
+  }
+}
+
+void BinaryTree::printLeaf() const
+{
+  leafPrinter(rootPtr);
+}
+
+void BinaryTree::leafPrinter(BinaryNode<int> *treePtr) const
+{
+  if (treePtr != nullptr)
+  {
+    if ((treePtr->getLeftChildPtr() == nullptr) && (treePtr->getRightChildPtr() == nullptr))
+    {
+      // found a leaf
+      std::cout << treePtr->getItem() << ' ';
+    }
+    else
+    {
+      leafPrinter(treePtr->getLeftChildPtr());
+      leafPrinter(treePtr->getRightChildPtr());
+    }
+  }
+}
+
+void BinaryTree::printTreeHeight() const
+{
+  int height = heightHelper(rootPtr) - 1;
+  std::cout << height;
+}
+
+int BinaryTree::heightHelper(BinaryNode<int> *subTreePtr) const
+{
+  if (subTreePtr == nullptr)
+  {
+    return 0;
+  }
+  else
+  {
+    return (1 + std::max(heightHelper(subTreePtr->getLeftChildPtr()), heightHelper(subTreePtr->getRightChildPtr())));
+  }
+}
+
+void BinaryTree::printNode(int &x)
+{
+  std::cout << x << ' ';
+}
+
+void BinaryTree::preorder() const
+{
+  preorderHelper(printNode, rootPtr);
+}
+
+void BinaryTree::preorderHelper(void visit(int &), BinaryNode<int> *treePtr) const
+{
+  if (treePtr != nullptr)
+  {
+    int item = treePtr->getItem();
+    visit(item);
+    preorderHelper(visit, treePtr->getLeftChildPtr());
+    preorderHelper(visit, treePtr->getRightChildPtr());
+  }
+}
+
+void BinaryTree::postorder() const
+{
+  postorderHelper(printNode, rootPtr);
+}
+
+void BinaryTree::postorderHelper(void visit(int &), BinaryNode<int> *treePtr) const
+{
+  if (treePtr != nullptr)
+  {
+    postorderHelper(visit, treePtr->getLeftChildPtr());
+    postorderHelper(visit, treePtr->getRightChildPtr());
+    int item = treePtr->getItem();
+    visit(item);
+  }
+}
+
+void BinaryTree::inorder() const
+{
+  inorderHelper(printNode, rootPtr);
+}
+
+void BinaryTree::inorderHelper(void visit(int &), BinaryNode<int> *treePtr) const
+{
+  if (treePtr != nullptr)
+  {
+    inorderHelper(visit, treePtr->getLeftChildPtr());
+    int item = treePtr->getItem();
+    visit(item);
+    inorderHelper(visit, treePtr->getRightChildPtr());
+  }
+}
+
+void BinaryTree::levelorder() const
+{
+  int height = heightHelper(rootPtr);
+  for (int i = 1; i <= height; i++)
+  {
+    printLevel(rootPtr, i);
+  }
+}
+
+void BinaryTree::printLevel(BinaryNode<int> *subTreePtr, int level) const
+{
+  if (subTreePtr == nullptr)
+  {
+    return;
+  }
+  if (level == 1)
+  {
+    std::cout << subTreePtr->getItem() << ' ';
+  }
+  else if (level > 1)
+  {
+    printLevel(subTreePtr->getLeftChildPtr(), level - 1);
+    printLevel(subTreePtr->getRightChildPtr(), level - 1);
+  }
+}
---
+++ dataStructures/lab05/Executive.cpp
@@ -0,0 +1,191 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    Executive.cpp
+*@date    09/25/19
+*@brief   Implementation file for Executive class, which creates and modifies a
+*         BinaryTree
+*******************************************************************************/
+
+#include "Executive.h"
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <limits>
+#include <fstream>
+#include <iostream>
+#include "BinaryTree.h"
+
+Executive::Executive(std::string fileName)
+{
+  std::ifstream inFile;
+  inFile.open(fileName);
+  if (inFile.is_open())
+  {
+    int temp;
+    while (inFile >> temp)
+    {
+      bool success = tree.addItem(temp);
+      if (!success)
+      {
+        throw std::runtime_error("ERROR: Invalid item in data file, please fix and run again");
+      }
+    }
+    inFile.close();
+    userInterface();
+  }
+  else
+  {
+    throw std::runtime_error("ERROR: Could not open file");
+  }
+}
+
+Executive::~Executive()
+{
+}
+
+void Executive::userInterface()
+{
+  int userInput = 0;
+  while (userInput != 11)
+  {
+    std::cout << "Please choose one of the following commands:\n";
+    std::cout << "1- Is tree full?\n2- Add item\n3- Delete\n4- Check if leaf\n";
+    std::cout << "5- Print leaves\n6- Print tree height\n7- Preorder\n";
+    std::cout << "8- Postorder\n9- Inorder\n10- Levelorder\n11- Exit\n>";
+    std::cin >> userInput;
+    std::cout << '\n';
+
+    //Validation of user input.
+
+    while (std::cin.fail())
+    {
+      std::cin.clear(); // unset failbit
+      // skip bad input up to the next newline
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+      std::cin >> userInput;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    //END OF VALIDATION
+
+    if (userInput == 1) //IsFull
+    {
+      if (tree.isFull())
+      {
+        std::cout << "True\n\n";
+      }
+      else
+      {
+        std::cout << "False\n\n";
+      }
+    }
+    else if (userInput == 2) //AddItem
+    {
+      int temp;
+      std::cout << "Please enter the value which you want to enter into the tree:\n>";
+      std::cin >> temp;
+
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> userInput;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      if (tree.addItem(temp))
+      {
+        std::cout << "Value inserted successfully!\n\n";
+      }
+      else
+      {
+        std::cout << "Failed to insert value\n\n";
+      }
+    }
+    else if (userInput == 3) //Delete
+    {
+      int pop = tree.remove();
+      if (pop == -1)
+      {
+        std::cout << "ERROR: Cannot delete root node, please insert more values before calling delete\n\n";
+      }
+      else
+      {
+        std::cout << "The last value is " << pop << ", and it has been deleted\n\n";
+      }
+    }
+    else if (userInput == 4) //Leaf
+    {
+      int temp;
+      std::cout << "Please enter the value which you want to test as a leaf node:\n>";
+      std::cin >> temp;
+
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> userInput;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      if (tree.leaf(temp))
+      {
+        std::cout << "The node with value " << temp << " is a leaf node\n\n";
+      }
+      else
+      {
+        std::cout << "The node with value " << temp << " is not a leaf node\n\n";
+      }
+    }
+    else if (userInput == 5) //PrintLeaf
+    {
+      std::cout << "The leaf nodes are: ";
+      tree.printLeaf();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 6) //PrintTreeHeight
+    {
+      std::cout << "The height of the tree is ";
+      tree.printTreeHeight();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 7) //Preorder
+    {
+      std::cout << "Printing the tree in preorder: ";
+      tree.preorder();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 8) //Postorder
+    {
+      std::cout << "Printing the tree in postorder: ";
+      tree.postorder();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 9) //Inorder
+    {
+      std::cout << "Printing the tree in inorder: ";
+      tree.inorder();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 10) //Levelorder
+    {
+      std::cout << "Printing the tree in levelorder: ";
+      tree.levelorder();
+      std::cout << "\n\n";
+    }
+    else if (userInput == 11) //Exit
+    {
+      std::cout << "Exiting...\n";
+    }
+    else //Invalid input
+    {
+      std::cout << "Please enter a valid menu option\n";
+    }
+    std::cout << "------------------------------------------------------\n";
+  }
+}
