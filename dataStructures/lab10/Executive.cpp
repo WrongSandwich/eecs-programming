ba502ca155948478d0f6c630aa27854065fa3d16
Evan Trout <fishyevan@gmail.com>
2019-11-11 09:07:18 -0600

    Add starter files for lab10

---
+++ dataStructures/lab10/Executive.cpp
@@ -0,0 +1,227 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    Executive.cpp
+*@date    10/24/19
+*@brief   Implementation file for Executive class, which creates and modifies a
+*         MinMaxHeap
+*******************************************************************************/
+
+#include "Executive.h"
+#include "SkewHeap.h"
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <limits>
+#include <fstream>
+#include <iostream>
+
+Executive::Executive(std::string fileName)
+{
+
+  std::ifstream inFile;
+  inFile.open(fileName);
+  if (inFile.is_open())
+  {
+    inFile.close();
+    skew.buildHeap(fileName);
+    userInterface();
+  }
+  else
+  {
+    throw std::runtime_error("ERROR: Could not open file");
+  }
+}
+
+Executive::~Executive()
+{
+}
+
+void Executive::userInterface()
+{
+  int userInput = 0;
+  while (userInput != 9)
+  {
+    std::cout << "Please choose one of the following commands:\n";
+    std::cout << "1- Insert\n2- Merge\n3- Delete\n4- Find\n";
+    std::cout << "5- Preorder\n6- Inorder\n7- Postorder\n";
+    std::cout << "8- Levelorder\n9- Exit\n>";
+    std::cin >> userInput;
+    std::cout << '\n';
+
+    //Validation of user input.
+
+    while (std::cin.fail())
+    {
+      std::cin.clear(); // unset failbit
+      // skip bad input up to the next newline
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+      std::cin >> userInput;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    //END OF VALIDATION
+
+    if (userInput == 1) //Insert
+    {
+      int temp;
+      std::cout << "Enter an integer value to be inserted into the min-skew heap:\n>";
+      std::cin >> temp;
+
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      if (skew.insert(temp))
+      {
+        std::cout << temp << " has been inserted successfully!\n";
+        if (skew.isSwapped())
+        {
+          std::cout << "Subtrees have been swapped. The new level order traversal is:\n";
+          skew.levelorder();
+          std::cout << "\n";
+        }
+      }
+      else
+      {
+        std::cout << "Failed to insert value " << temp << "\n";
+      }
+    }
+    else if (userInput == 2) //Merge
+    {
+      int temp1, temp2, temp3;
+      std::cout << "Enter the three elements for the tree H2:\n";
+      std::cin >> temp1;
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp1;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cin >> temp2;
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp2;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cin >> temp3;
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp3;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      if (!skew.isPresent(temp1) && !skew.isPresent(temp2) && !skew.isPresent(temp3))
+      {
+        BinaryNode<int>* temp1Node = new BinaryNode<int>(temp1);
+        BinaryNode<int>* temp2Node = new BinaryNode<int>(temp2);
+        BinaryNode<int>* temp3Node = new BinaryNode<int>(temp3);
+        temp1Node = skew.merge(temp1Node, temp2Node);
+        temp1Node = skew.merge(temp1Node, temp3Node);
+        skew.mergeToRoot(temp1Node);
+        std::cout << "Trees H1 and H2 have been merged to form new tree H3. The new level order traversal for tree H3 is:\n";
+        skew.levelorder();
+        std::cout << "\n";
+      }
+      else
+      {
+        std::cout << "Merge was unsuccessful. Make sure none of your elements are already present in the tree.\n";
+      }
+    }
+    else if (userInput == 3) //Delete
+    {
+      int temp;
+      std::cout << "Enter the element to be deleted:\n>";
+      std::cin >> temp;
+
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      if (skew.remove(temp))
+      {
+        std::cout << "The element " << temp << " was successfully deleted from the min-skew heap\n";
+      }
+      else
+      {
+        std::cout << "Failed to delete the element " << temp << " from the min-skew heap\n";
+      }
+    }
+    else if (userInput == 4) //Find
+    {
+      int temp;
+      std::cout << "Enter the element to search for:\n>";
+      std::cin >> temp;
+
+      while (std::cin.fail())
+      {
+        std::cin.clear(); // unset failbit
+        // skip bad input up to the next newline
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Sorry, your input did not seem to be an int. Try again: ";
+        std::cin >> temp;
+      }
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      if (skew.isPresent(temp))
+      {
+        std::cout << "The element " << temp << " has been found in the min-skew heap\n";
+      }
+      else
+      {
+        std::cout << "Failed to find the element " << temp << " in the min-skew heap\n";
+      }
+    }
+    else if (userInput == 5) //Preorder
+    {
+      skew.preorder();
+      std::cout << "\n";
+    }
+    else if (userInput == 6) //Inorder
+    {
+      skew.inorder();
+      std::cout << "\n";
+    }
+    else if (userInput == 7) //Postorder
+    {
+      skew.postorder();
+      std::cout << "\n";
+    }
+    else if (userInput == 8) //Levelorder
+    {
+      skew.levelorder();
+      std::cout << "\n";
+    }
+    else if (userInput == 9) //Exit
+    {
+      std::cout << "Exiting...\n";
+    }
+    else //Invalid input
+    {
+      std::cout << "Please enter a valid menu option\n";
+    }
+    std::cout << "------------------------------------------------------\n";
+  }
+}
---
+++ dataStructures/lab10/SkewHeap.h
@@ -0,0 +1,166 @@
+/*******************************************************************************
+*@author  Evan Trout
+*@file    SkewHeap.h
+*@date    11/08/19
+*@brief   Header file for SkewHeap class, which controls a tree of linked
+*         BinaryNodes and allows for various operations on them.
+*******************************************************************************/
+
+#ifndef SKEW_HEAP
+#define SKEW_HEAP
+
+#include "BinaryNode.h"
+#include <string>
+
+class SkewHeap {
+private:
+  BinaryNode<int>* rootPtr; //root of the heap
+  bool swapped; //boolean tracking whether or not subtrees have been swapped
+public:
+  /**
+  * @pre constructor is called
+  * @return initalized SkewHeap
+  **/
+  SkewHeap();
+
+  /**
+  * @pre Destructor is called
+  * @post Calls destroyTree on rootPtr
+  **/
+  ~SkewHeap();
+
+  /**
+  * @pre called with a valid, tested file name
+  * @post builds the heap from the file of data using insert calls
+  * @param fileName: name of file containing data
+  **/
+  void buildHeap(std::string fileName);
+
+  /**
+  * @post creates a new node with the given element and calls merge
+  * @param x: element to be inserted
+  * @return true if successful, otherwise false
+  **/
+  bool insert(int x);
+
+  /**
+  * @post finds and removes the given element from the heap
+  * @param x: element to be removed
+  * @return true if successful, otherwise false
+  **/
+  bool remove(int x);
+
+  /**
+  * @post merges the two given heaps recursively
+  * @param heap1/heap2: two heaps to be merged
+  * @return root of newly merged heap
+  **/
+  BinaryNode<int>* merge(BinaryNode<int>* heap1, BinaryNode<int>* heap2);
+
+  /**
+  * @pre called by destructor
+  * @post Recursively deletes all nodes
+  * @param subTreePtr: node to delete
+  **/
+  void destroyTree(BinaryNode<int>* subTreePtr);
+
+  /**
+  * @return boolean of whether or not tree is empty
+  **/
+  bool isEmpty() const;
+
+  /**
+  * @post recursively determines the height of the current subtree
+  * @param subTreePtr: current location in tree
+  * @return height of subtree
+  **/
+  int heightHelper(BinaryNode<int> *subTreePtr) const;
+
+  /**
+  * @pre called in a traversal
+  * @post prints the value of x
+  * @param x: value to be printed
+  **/
+  static void printNode(int& x);
+
+  /**
+  * @post calls preorderHelper and passes printNode function and rootPtr
+  **/
+  void preorder() const;
+
+  /**
+  * @pre called by itself or preorder
+  * @post recursively prints values in preorder
+  * @param visit: function that will be run on the node
+  * @param treePtr: current node
+  **/
+  void preorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
+
+  /**
+  * @post calls postorderHelper and passes printNode function and rootPtr
+  **/
+  void postorder() const;
+
+  /**
+  * @pre called by itself or postorder
+  * @post recursively prints values in postorder
+  * @param visit: function that will be run on the node
+  * @param treePtr: current node
+  **/
+  void postorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
+
+  /**
+  * @post calls inorderHelper and passes printNode function and rootPtr
+  **/
+  void inorder() const;
+
+  /**
+  * @pre called by itself or inorder
+  * @post recursively prints values in inorder
+  * @param visit: function that will be run on the node
+  * @param treePtr: current node
+  **/
+  void inorderHelper(void visit(int&), BinaryNode<int>* treePtr) const;
+
+  /**
+  * @post calls levelorderHelper and passes printNode function and rootPtr
+  **/
+  void levelorder() const;
+
+  /**
+  * @pre called by levelorder or by itself
+  * @post recursively prints the nodes in levelorder
+  * @param subTreePtr: current node
+  * @param level: current level
+  **/
+  void printLevel(BinaryNode<int>* subTreePtr, int level) const;
+
+  /**
+  * @post returns the current state of swapped and then sets it back to false
+  * @return the current state of swapped
+  **/
+  bool isSwapped();
+
+  /**
+  * @post calls itself recursively to find the parent of the given element
+  * @param curPtr: current location in tree
+  * @param x: element being searched for
+  * @return pointer of x's parent, otherwise nullptr
+  **/
+  BinaryNode<int>* findElement(BinaryNode<int>* curPtr, int x);
+
+  /**
+  * @post calls findElement on rootPtr and then returns a bool of if the element was found
+  * @param x: element being searched for
+  * @return true if x was found, otherwise false
+  **/
+  bool isPresent(int x);
+  
+  /**
+  * @post merges the newHeap into rootPtr
+  * @param newHeap: heap to be merged into rootPtr
+  **/
+  void mergeToRoot(BinaryNode<int>* newHeap);
+};
+
+#endif
